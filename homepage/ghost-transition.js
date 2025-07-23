6601c80ff50147518deb69cb6ce2eacee4cedb97
Edward Kelly <madzenejk@gmail.com>
2025-07-22 23:01:54 -0500

    Update under construction page to use skeleton image

--- homepage/ghost-transition.js
+++ homepage/ghost-transition.js
@@ -1,8 +1,10 @@
 function triggerGhostTransition(targetUrl) {
   const overlay = document.getElementById('ghost-overlay');
+  overlay.style.pointerEvents = 'auto';
   overlay.style.bottom = '0'; // Swipe up
 
   setTimeout(() => {
+    overlay.style.pointerEvents = 'none';
     window.location.href = targetUrl;
   }, 1000); // Match this to the duration of your swipe
 }
